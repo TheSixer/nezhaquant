1c1e91c98b0ab3f78c467a08e4cb1e4106c19b7e
L.J.Zhang <longjia3713@163.com>
2025-03-06 17:40:06 +1100

    feat: test

--- src/types.d.ts
+++
@@ -1,9 +0,0 @@
-interface I18nItem {
-  cover?: string
-  title: string
-  description: string[]
-}
-
-interface I18nObject extends I18nItem {
-  items?: I18nItem[]
-}
--- src/utils/classnames.ts
+++
@@ -1,6 +0,0 @@
-import { ClassValue, clsx } from 'clsx'
-import { twMerge } from 'tailwind-merge'
-
-export function classnames(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
-}
